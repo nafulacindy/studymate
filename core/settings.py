893a21c4c55f99f3f41d32edbfa1415181b7eb1d
nafulacindy <cindytalia022@gmail.com>
2025-08-30 16:23:00 +0300

    Prepare for Render deployment

--- core/settings.py
+++ core/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-ALLOWED_HOSTS = ["https://studymate-ehuh.onrender.com/", 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["studymate-ehuh.onrender.com/", 'localhost', '127.0.0.1']
 
 
 
