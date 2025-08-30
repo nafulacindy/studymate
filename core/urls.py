1994cc5abd762976d3e4fe36c5b74b5aec27408a
nafulacindy <cindytalia022@gmail.com>
2025-08-30 17:06:20 +0300

    Prepare for Render deployment

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to StudyMate!")
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/tutoring/', include('tutoring.urls')),
